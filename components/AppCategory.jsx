c6b20d8d746f4b6d2cf55d87581e010fd3db85f9
Duvan Mancilla <Duvanred0@gmail.com>
2024-08-11 21:51:20 -0500

    descargar apps

--- components/AppCategory.jsx
+++ components/AppCategory.jsx
@@ -11,6 +11,12 @@ function AppCategory({ category }) {
 
   const filteredApps = appsInfo.filter(app => app.category === category).slice(0,10)
 
+  const handleDownloadChange = (url) => {
+    if (url) {
+      window.location.href = url;
+    }
+  };
+
   return (
     <div className="flex justify-between flex-wrap max-w-[1050px] m-auto">
       {filteredApps.map(app => (
@@ -28,7 +34,7 @@ function AppCategory({ category }) {
           </div>
           <p className="text-[14px] md:text-sm font-Roboto font-normal lowercase block md:hidden">{app.description}</p>
           <div className="bottom flex justify-between mt-2">
-            <select name="select" className="bg-black text-white font-Roboto font-bold text-[12px] md:px-2 pr-6 md:pr-14 py-1 rounded-sm">
+            <select name="select" className="bg-black text-white font-Roboto font-bold text-[12px] md:px-2 pr-6 md:pr-14 py-1 rounded-sm" onChange={(e) => handleDownloadChange(e.target.value)}>
               <option value="" disabled selected>Descargar</option>
               {app.windows && <option value={app.windows}>Windows</option>}
               {app.macOS && <option value={app.macOS}>Mac</option>}
